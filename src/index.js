4bc57dedcc37fca8660d99dd77ad5cc7e7daefb6
ali-fr-2 <alifarjamy2@gmail.com>
2025-11-06 19:00:05 +0330

    part7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// git add .
+// git commit -m "توضیح تغییرات"
+// git push 
+
+class Timer extends React.Component {
+  render() {
+    return <h2>it is {new Date().toLocaleTimeString()}</h2>;
+  }
+}
+class Hello extends React.Component {
+  render() {
+    return <h1>hello my friends</h1>;
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Timer />
+      </div>
+    );
+  }
+}
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+setInterval(()=>{
+    root.render(<App/>)
+},1000)
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render();
