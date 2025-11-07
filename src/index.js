d03b1402e996fa5d8b63c0a1a767886f32cfa29e
ali-fr-2 <alifarjamy2@gmail.com>
2025-11-07 18:26:35 +0330

    part9

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+import "./style.css";
 // git add .
 // git commit -m "توضیح تغییرات"
-// git push 
+// git push
 
 class Timer extends React.Component {
-  render() {
-    return <h2>it is {new Date().toLocaleTimeString()}</h2>;
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+  // componentDidMount فقط یک‌بار بعد از render اولیه اجرا می‌شه، و این دقیقاً همون چیزیه که برای تایمر لازمه
+  componentDidMount(){
+    this.interval =setInterval(() => {
+      this.setState({
+        time :new Date().toLocaleTimeString(),
+      })
+    }, 1000);
+  }
+  // در componentWillUnmount تایمر پاک می‌شه تا نشت حافظه نداشته باشی.
+  componentWillUnmount(){
+    clearInterval(this.setInterval);
+  }
+  render(){
+    return(
+      <h2 className="timer">
+        it is {this.state.time}
+      </h2>
+    )
   }
 }
+
 class Hello extends React.Component {
   render() {
     return <h1>hello my friends</h1>;
@@ -19,7 +42,7 @@ class Hello extends React.Component {
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="main">
         <Hello />
         <Timer />
       </div>
@@ -27,10 +50,8 @@ class App extends React.Component {
   }
 }
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-setInterval(()=>{
-    root.render(<App/>)
-},1000)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render();
