eda6bae96891cb37b88bd2f4bd5d82e02dddf83b
ali-fr-2 <alifarjamy2@gmail.com>
2025-12-26 12:42:38 +0330

    part31

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,43 +1,46 @@
-import React, { useContext } from 'react';
-import { MainContext } from './contexts/MainContext';
-import Gallery from './gallery/Gallery';
-import Posts from './posts/Posts';
-import style from './style.module.css'
-import Todos from './todos/Todos';
-import Users from './users/Users';
-import { Route, Routes } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { MainContext } from "./contexts/MainContext";
+import Gallery from "./gallery/Gallery";
+import Posts from "./posts/Posts";
+import style from "./style.module.css";
+import Todos from "./todos/Todos";
+import Users from "./users/Users";
+import { Navigate, replace, Route, Routes } from "react-router-dom";
 
-const Content = ()=>{
+const Content = () => {
+  const { showMenu, setShowMenu } = useContext(MainContext);
+//   const [IsUser, SetIsUser] = useState(false);
 
-    const {showMenu,setShowMenu} = useContext(MainContext)
+  const handleShowMenu = (event) => {
+    event.stopPropagation();
+    // این خط بالا یعنی از المان والد پیروی نکن
+    // مشکل خط 22
+    setShowMenu(!showMenu);
+    console.log(showMenu);
+  };
 
-    const handleShowMenu = (event)=>{
-        event.stopPropagation()
-        // این خط بالا یعنی از المان والد پیروی نکن 
-        // مشکل خط 22
-        setShowMenu(!showMenu)
-        console.log(showMenu);
-    }
-
-    return (
-        <div className={style.content_section} onClick={()=>{setShowMenu(false)}}>
-            {/* این جا قصد اینه که هر جای صفحع را کیلک کردیم منو بسته بشه اما مشکل این بود که خود دکمه هم کار نمیکرد و از المان والد پیروی میکرد */}
-            <i className={`${style.menu_button} fas fa-bars text-dark m-2 pointer`} 
-            onClick={handleShowMenu}
-            ></i>
-            <Routes>
-                <Route path='/' element={<Users/>}/>
-                <Route path='/posts' element={<Posts/>}/>
-                <Route path='/gallery' element={<Gallery/>}/>
-                <Route path='/todos' element={<Todos/>}/>
-            </Routes>
-            
-               
-                
-                
-        </div>
-    )
-
-}
+  return (
+    <div
+      className={style.content_section}
+      onClick={() => {
+        setShowMenu(false);
+      }}
+    >
+      {/* این جا قصد اینه که هر جای صفحع را کیلک کردیم منو بسته بشه اما مشکل این بود که خود دکمه هم کار نمیکرد و از المان والد پیروی میکرد */}
+      <i
+        className={`${style.menu_button} fas fa-bars text-dark m-2 pointer`}
+        onClick={handleShowMenu}
+      ></i>
+      <Routes>
+        {/* <Route path="/" element={IsUser ? <Users/> : <Navigate replace to ="/posts"/> } /> */}
+        <Route path="/users" element={<Users />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="*" element={<Users />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default Content;
