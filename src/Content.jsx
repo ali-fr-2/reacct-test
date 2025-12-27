f341f96ce2f3ae298c05f604e7c69e637ecfe637
ali-fr-2 <alifarjamy2@gmail.com>
2025-12-27 13:09:52 +0330

    part32

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,12 +4,14 @@ import Gallery from "./gallery/Gallery";
 import Posts from "./posts/Posts";
 import style from "./style.module.css";
 import Todos from "./todos/Todos";
+import AddUser from "./users/AddUser";
+import EditDesc from "./users/EditDesk";
 import Users from "./users/Users";
 import { Navigate, replace, Route, Routes } from "react-router-dom";
 
 const Content = () => {
   const { showMenu, setShowMenu } = useContext(MainContext);
-//   const [IsUser, SetIsUser] = useState(false);
+  //   const [IsUser, SetIsUser] = useState(false);
 
   const handleShowMenu = (event) => {
     event.stopPropagation();
@@ -28,12 +30,16 @@ const Content = () => {
     >
       {/* این جا قصد اینه که هر جای صفحع را کیلک کردیم منو بسته بشه اما مشکل این بود که خود دکمه هم کار نمیکرد و از المان والد پیروی میکرد */}
       <i
-        className={`${style.menu_button} fas fa-bars text-dark m-2 pointer`}
+        className={`${style.menu_button} fas fa-bars text-dark m-2 pointer d-md-none`}
         onClick={handleShowMenu}
       ></i>
       <Routes>
         {/* <Route path="/" element={IsUser ? <Users/> : <Navigate replace to ="/posts"/> } /> */}
         <Route path="/users" element={<Users />} />
+        <Route path="/users/add" element={<AddUser />}>
+          <Route path=":UserId" />
+          {/* <Route path=":userId" element={<EditDesc/>} /> */}
+        </Route>
         <Route path="/posts" element={<Posts />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/todos" element={<Todos />} />
